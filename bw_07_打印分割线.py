f71728a7af566f0446aecdbb207e1c1fcd61a90d
benslwang <363111516@qq.com>
2022-04-24 16:03:24 +0800

    1

--- bw_07_打印分割线.py
+++
@@ -1,5 +0,0 @@
-def print_line(char, times):  # char表示一个字节
-    print(char * times)
-
-
-print_line("-", 60)
---
+++ bw_07_打印多行分割线.py
@@ -0,0 +1,11 @@
+# 打印5行分割线
+print_times = 0
+while print_times < 5:
+    def print_line(char, times):  # char表示一个字节
+        print(char * times)
+
+
+    print_line("-", 60)
+    print_times += 1
+
+# 打印5行分割线
