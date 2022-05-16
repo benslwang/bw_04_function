99e0462324efe3744cd7d42c28bd8d747575e4f9
benslwang <363111516@qq.com>
2022-05-17 00:16:37 +0800

    P286-296

---
+++ bw_23_公共方法.py
@@ -0,0 +1,15 @@
+# in/not in
+print('a' in 'abcd')
+print('a' not in 'abcd')
+print(1 in [0, 1, 2])
+print(1 not in [0, 1, 2])
+print("a" in {"a": "laowang"})
+print("laowang" in {"a": "laowang"})
+
+"""
+完整的for循环语法
+for 变量 in 集合
+    循环体代码
+else:
+    没有通过break退出循环，循环结束后，会执行的代码
+"""
